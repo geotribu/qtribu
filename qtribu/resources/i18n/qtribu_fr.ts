67827b8aa1ce7f4c6eb51e3d2cd305a7136d2569
Julien M <julien.moura@oslandia.com>
2022-08-17 15:55:16 +0200

    Translate new files

--- qtribu/resources/i18n/qtribu_fr.ts
+++ qtribu/resources/i18n/qtribu_fr.ts
@@ -2,53 +2,108 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr" sourcelanguage="en">
 <context>
+    <name>AuthorInformationsWidget</name>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="41"/>
+        <source>Authoring information</source>
+        <translation>Informations d&apos;attribution</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="56"/>
+        <source>Firstname:</source>
+        <translation>Prénom :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="79"/>
+        <source>Lastname:</source>
+        <translation>Nom :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="105"/>
+        <source>Email:</source>
+        <translation>Email :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="125"/>
+        <source>Used to credit the commit author.</source>
+        <translation>Utilisé pour attribuer le commit à l&apos;auteur/e.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="128"/>
+        <source>GitHub account:</source>
+        <translation>GitHub :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="165"/>
+        <source>Used during broadcasting to credit author.</source>
+        <translation>Utilisé pour créditer l&apos;auteur/e au moment de la diffusion.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="148"/>
+        <source>LinkedIn account:</source>
+        <translation>LinkedIn :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/wdg_authoring.ui" line="168"/>
+        <source>Twitter account:</source>
+        <translation>Twitter :</translation>
+    </message>
+</context>
+<context>
     <name>ConfigOptionsPage</name>
     <message>
-        <location filename="../../gui/dlg_settings.py" line="127"/>
+        <location filename="../../gui/dlg_settings.py" line="129"/>
         <source>Read history has been reset.</source>
-        <translation>L'historique de lecture a été réinitialisé.</translation>
+        <translation>L&apos;historique de lecture a été réinitialisé.</translation>
     </message>
 </context>
 <context>
     <name>GeotribuPlugin</name>
     <message>
-        <location filename="../../plugin_main.py" line="246"/>
+        <location filename="../../plugin_main.py" line="256"/>
         <source>Newest article</source>
         <translation>Afficher le dernier article</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="74"/>
+        <location filename="../../plugin_main.py" line="85"/>
         <source>Help</source>
         <translation>Aide en ligne</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="83"/>
+        <location filename="../../plugin_main.py" line="94"/>
         <source>Settings</source>
         <translation>Réglages</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="200"/>
+        <location filename="../../plugin_main.py" line="213"/>
         <source>New content published:</source>
         <translation>Nouveau contenu publié :</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="105"/>
+        <location filename="../../plugin_main.py" line="117"/>
         <source>QGIS forum on GeoRezo</source>
         <translation>Forum QGIS sur GeoRezo</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="117"/>
+        <location filename="../../plugin_main.py" line="129"/>
         <source>Geotribu website</source>
         <translation>Site Geotribu</translation>
     </message>
     <message>
-        <location filename="../../plugin_main.py" line="129"/>
+        <location filename="../../plugin_main.py" line="141"/>
         <source>OSGeo France</source>
         <translation>OSGeo FR</translation>
     </message>
-</context>
-<context>
-    <name>PlgTranslator</name>
+    <message>
+        <location filename="../../plugin_main.py" line="78"/>
+        <source>Propose a news to the next GeoRDP</source>
+        <translation>Proposer une news pour la GeoRDP</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="229"/>
+        <source>Michel, we&apos;ve got a problem: {err}</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>SplashChanger</name>
@@ -81,6 +136,79 @@
         <location filename="../../logic/splash_changer.py" line="115"/>
         <source>Splash screen: custom applied. Please, restart.</source>
         <translation>Splash screen personnalisé appliqué. Merci de redémarrer.</translation>
+    </message>
+</context>
+<context>
+    <name>dlg_form_rdp_news</name>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="14"/>
+        <source>GeoRDP - News Form</source>
+        <translation>GeoRDP - Formulaire de soumission de news</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="34"/>
+        <source>The news</source>
+        <translation>Votre news</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="50"/>
+        <source>The title must be concise and avoid some special characters, especially at the end</source>
+        <translation>Le titre doit être concis et éviter certains caractères spéciaux, notamment à la fin</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="53"/>
+        <source>Title:</source>
+        <translation>Titre :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="63"/>
+        <source>Icon:</source>
+        <translation>Icône :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="84"/>
+        <source>Category:</source>
+        <translation>Catégorie :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="94"/>
+        <source>Keywords:</source>
+        <translation>Mots-clés :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="115"/>
+        <source>Body:</source>
+        <translation>Corps de la news :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="151"/>
+        <source>Publication</source>
+        <translation>Transparence et publication</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="164"/>
+        <source>License:</source>
+        <translation>Licence :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="171"/>
+        <source>I accept that my contribution is published under the CC BY-NC-SA 4.0</source>
+        <translation>J&apos;accepte que ma contribution soit publiée en CC BY-NC-SA 4.0</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="188"/>
+        <source>Comment:</source>
+        <translation>Commentaire libre :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="195"/>
+        <source>Transparency:</source>
+        <translation>Transparence :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/form_rdp_news.ui" line="202"/>
+        <source>I&apos;m not related to thepublished content. If not, I give some details in the comment area.</source>
+        <translation>Je ne fais pas d&apos;autopromotion. Si c&apos;est le cas, je donne des détails en commentaire.</translation>
     </message>
 </context>
 <context>
@@ -126,39 +254,39 @@
         <translation>Activer la notification push</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="287"/>
+        <location filename="../../gui/dlg_settings.ui" line="290"/>
         <source>Miscellaneous</source>
         <translation>Divers</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="308"/>
+        <location filename="../../gui/dlg_settings.ui" line="311"/>
         <source>Enable debug mode.</source>
         <translation>Activer le mode DEBUG.</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="317"/>
+        <location filename="../../gui/dlg_settings.ui" line="320"/>
         <source>Debug mode (degraded performances)</source>
         <translation>Mode DEBUG (performances dégradées)</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="339"/>
+        <location filename="../../gui/dlg_settings.ui" line="342"/>
         <source>Version used to save settings:</source>
         <translation>Version des paramètres :</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="399"/>
+        <location filename="../../gui/dlg_settings.ui" line="402"/>
         <source>Report an issue</source>
         <translation>Créer un ticket</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="380"/>
+        <location filename="../../gui/dlg_settings.ui" line="383"/>
         <source>Help</source>
         <translation>Aide en ligne</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_settings.ui" line="128"/>
         <source>Reset read history</source>
-        <translation>Effacer l'historique</translation>
+        <translation>Effacer l&apos;historique</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_settings.ui" line="137"/>
@@ -168,7 +296,7 @@
     <message>
         <location filename="../../gui/dlg_settings.ui" line="144"/>
         <source>&apos;0&apos; means unlimited time, waiting a manual close from the end-user.</source>
-        <translation>'0' signifie une durée illimitée, l'utilisateur final devra fermer la notification manuellement.</translation>
+        <translation>&apos;0&apos; signifie une durée illimitée, l&apos;utilisateur final devra fermer la notification manuellement.</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_settings.ui" line="156"/>
