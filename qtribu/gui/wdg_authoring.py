c7c3d2ee86dd1483bb2c6ef4f2aa898f8458a7cd
Julien M <julien.moura@oslandia.com>
2022-08-20 23:22:26 +0200

    Add validators on line edit

--- qtribu/gui/wdg_authoring.py
+++ qtribu/gui/wdg_authoring.py
@@ -6,6 +6,7 @@ from qgis.PyQt import uic
 from qgis.PyQt.QtWidgets import QWidget
 
 # plugin
+from qtribu.gui.gui_commons import QVAL_EMAIL, QVAL_URL
 from qtribu.toolbelt import PlgLogger, PlgOptionsManager
 
 
@@ -13,7 +14,7 @@ class AuthoringWidget(QWidget):
     def __init__(self, parent: QWidget = None):
         """QWidget to set user informations.
 
-        :param parent: _description_
+        :param parent: parent widget or application
         :type parent: QWidget
         """
         super().__init__(parent)
@@ -21,6 +22,13 @@ class AuthoringWidget(QWidget):
         self.plg_settings = PlgOptionsManager()
         uic.loadUi(Path(__file__).parent / "{}.ui".format(Path(__file__).stem), self)
 
+        # check inputs
+        self.lne_email.setValidator(QVAL_EMAIL)
+        self.lne_github_account.setValidator(QVAL_URL)
+        self.lne_linkedin_account.setValidator(QVAL_URL)
+        self.lne_twitter_account.setValidator(QVAL_URL)
+
+        # fill fields from saved settings
         self.load_settings()
 
     def load_settings(self) -> dict:
