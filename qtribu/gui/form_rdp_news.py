67bd91f05b878dc0b855a39fd5bcc7ea1f847db6
Julien M <julien.moura@oslandia.com>
2022-08-20 23:12:00 +0200

    Add icon combobox logic

--- qtribu/gui/form_rdp_news.py
+++ qtribu/gui/form_rdp_news.py
@@ -1,22 +1,26 @@
 # standard
 from functools import partial
 from pathlib import Path
+from urllib.parse import urlparse
 
 # PyQGIS
 from qgis.PyQt import uic
-from qgis.PyQt.Qt import QUrl
-from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtCore import Qt, QUrl
 from qgis.PyQt.QtGui import QDesktopServices
 from qgis.PyQt.QtWidgets import QDialog
 
 # plugin
-from qtribu.constants import GEORDP_NEWS_CATEGORIES
-from qtribu.toolbelt import PlgLogger, PlgOptionsManager
+from qtribu.constants import GEORDP_NEWS_CATEGORIES, GEORDP_NEWS_ICONS
+from qtribu.toolbelt import NetworkRequestsManager, PlgLogger, PlgOptionsManager
 
 
 class RdpNewsForm(QDialog):
+    """QDialog form to submit a news to a next GeoRDP."""
+
+    LOCAL_CDN_PATH: Path = Path().home() / ".geotribu/cdn/"
+
     def __init__(self, parent=None):
-        """QDialog to set user informations.
+        """Constructor.
 
         :param parent: parent widget or application
         :type parent: QWidget
@@ -26,6 +30,7 @@ class RdpNewsForm(QDialog):
 
         self.log = PlgLogger().log
         self.plg_settings = PlgOptionsManager()
+        self.qntwk = NetworkRequestsManager()
 
         # populate combobox of news category
         for rdp_category in GEORDP_NEWS_CATEGORIES:
@@ -34,6 +39,19 @@ class RdpNewsForm(QDialog):
                 rdp_category.order - 1, rdp_category.description, Qt.ToolTipRole
             )
 
+        # populate combobox of news icons
+        self.cbb_icon.addItem("", None)
+        for rdp_icon in GEORDP_NEWS_ICONS:
+            if rdp_icon.kind != "icon":
+                continue
+            self.cbb_icon.addItem(rdp_icon.name, rdp_icon.url)
+            self.cbb_icon.setItemData(
+                GEORDP_NEWS_ICONS.index(rdp_icon) + 1,
+                rdp_icon.description,
+                Qt.ToolTipRole,
+            )
+        self.cbb_icon.textActivated.connect(self.cbb_icon_selected)
+
         # connect help button
         self.btn_box.helpRequested.connect(
             partial(
@@ -41,3 +59,17 @@ class RdpNewsForm(QDialog):
                 QUrl("https://static.geotribu.fr/contribuer/rdp/add_news/"),
             )
         )
+
+    def cbb_icon_selected(self) -> None:
+        """Download selected icon locally if it doesn't exist already."""
+
+        icon_remote_url = self.cbb_icon.currentData()
+        icon_remote_url_parsed = urlparse(icon_remote_url)
+        icon_local_path = Path(self.LOCAL_CDN_PATH / icon_remote_url_parsed.path[1:])
+        if not icon_local_path.is_file():
+            self.log(message=f"icon not exists: {icon_local_path}", log_level=4)
+            icon_local_path.parent.mkdir(parents=True, exist_ok=True)
+            self.qntwk.download_file(
+                remote_url=icon_remote_url,
+                local_path=str(icon_local_path.resolve()),
+            )
