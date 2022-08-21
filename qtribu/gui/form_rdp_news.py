b3a98c7c8a5866ddbc1a7146145619a822ebcebf
Julien M <julien.moura@oslandia.com>
2022-08-21 21:42:53 +0200

    Add preview logic

--- qtribu/gui/form_rdp_news.py
+++ qtribu/gui/form_rdp_news.py
@@ -1,9 +1,18 @@
+#! python3  # noqa: E265
+
+"""
+    Form to submit a news for a GeoRDP.
+TODO: markdown highlight https://github.com/rupeshk/MarkdownHighlighter/blob/master/editor.py
+https://github.com/baudren/NoteOrganiser/blob/devel/noteorganiser/syntax.py
+"""
+
 # standard
 from functools import partial
 from pathlib import Path
 from urllib.parse import urlparse
 
 # PyQGIS
+from qgis.core import QgsApplication
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import Qt, QUrl
 from qgis.PyQt.QtGui import QDesktopServices, QIcon
@@ -36,6 +45,9 @@ class RdpNewsForm(QDialog):
         # custom icon
         self.setWindowIcon(QIcon(str(DIR_PLUGIN_ROOT / "resources/images/news.png")))
 
+        # title
+        self.lne_title.textChanged.connect(self.auto_preview)
+
         # populate combobox of news category
         for rdp_category in GEORDP_NEWS_CATEGORIES:
             self.cbb_category.addItem(rdp_category.name)
@@ -48,13 +60,20 @@ class RdpNewsForm(QDialog):
         for rdp_icon in GEORDP_NEWS_ICONS:
             if rdp_icon.kind != "icon":
                 continue
-            self.cbb_icon.addItem(rdp_icon.name, rdp_icon.url)
+            self.cbb_icon.addItem(rdp_icon.name, rdp_icon)
             self.cbb_icon.setItemData(
                 GEORDP_NEWS_ICONS.index(rdp_icon) + 1,
                 rdp_icon.description,
                 Qt.ToolTipRole,
             )
         self.cbb_icon.textActivated.connect(self.cbb_icon_selected)
+
+        # connect preview button
+        self.btn_preview.setIcon(
+            QIcon(QgsApplication.iconPath("mActionShowAllLayersGray.svg"))
+        )
+        self.btn_preview.clicked.connect(self.generate_preview)
+        self.txt_preview.setStyleSheet("background-color:transparent;")
 
         # connect help button
         self.btn_box.helpRequested.connect(
@@ -66,8 +85,11 @@ class RdpNewsForm(QDialog):
 
     def cbb_icon_selected(self) -> None:
         """Download selected icon locally if it doesn't exist already."""
+        selected_icon: GeotribuImage = self.cbb_icon.currentData()
+        if not selected_icon:
+            return
 
-        icon_remote_url = self.cbb_icon.currentData()
+        icon_remote_url = selected_icon.url
         icon_remote_url_parsed = urlparse(icon_remote_url)
         icon_local_path = Path(self.LOCAL_CDN_PATH / icon_remote_url_parsed.path[1:])
         if not icon_local_path.is_file():
@@ -77,3 +99,34 @@ class RdpNewsForm(QDialog):
                 remote_url=icon_remote_url,
                 local_path=str(icon_local_path.resolve()),
             )
+
+        self.auto_preview()
+
+    def auto_preview(self) -> None:
+        """To be connected to input widgets. Triggers the preview if the related
+        checkbox is checked.
+        """
+        if self.chb_auto_preview.isChecked():
+            self.generate_preview()
+
+    def generate_preview(self) -> None:
+        """Render news in the preview area."""
+        # title
+        md_txt = f"### {self.lne_title.text()}\n"
+
+        # icon
+        selected_icon: GeotribuImage = self.cbb_icon.currentData()
+        if selected_icon:
+            icon_remote_url_parsed = urlparse(selected_icon.url)
+            icon_local_path = Path(
+                self.LOCAL_CDN_PATH / icon_remote_url_parsed.path[1:]
+            )
+            md_txt += f"\n![selected_icon.description]({icon_local_path})\n"
+        else:
+            md_txt += "\n"
+
+        md_txt += f"\n{self.txt_body.toPlainText()}\n"
+
+        # show it
+        self.txt_preview.clear()
+        self.txt_preview.setMarkdown(md_txt)
