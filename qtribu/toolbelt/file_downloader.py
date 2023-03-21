93bb1ed28e924f6f935821c439c621c112bac53f
Julien M <julien.moura@oslandia.com>
2023-03-21 11:33:09 +0100

    Add file downloader

---
+++ qtribu/toolbelt/file_downloader.py
@@ -0,0 +1,51 @@
+#! python3  # noqa: E265
+
+"""
+    Download remote files.
+"""
+
+# Standard library
+import logging
+
+# PyQGIS
+from qgis.core import QgsFileDownloader
+from qgis.PyQt.QtCore import QEventLoop, QUrl
+
+# project
+from qtribu.toolbelt.log_handler import PlgLogger
+
+# ############################################################################
+# ########## Globals ###############
+# ##################################
+
+plg_logger = PlgLogger()
+
+# ############################################################################
+# ########## Functions #############
+# ##################################
+
+
+def get_from_http(uri: str, output_path: str) -> str:
+    """Download a file from a remote web server accessible through HTTP.
+
+    :param uri: web URL to the file to download
+    :type uri: str
+    :param output_path: path to the local file
+    :type output_path: str
+
+    :return: output path
+    :rtype: str
+    """
+    plg_logger.log(f"Downloading file from {uri} to {output_path}")
+
+    # download it
+    loop = QEventLoop()
+    project_download = QgsFileDownloader(
+        url=QUrl(uri), outputFileName=output_path, delayStart=True
+    )
+    project_download.downloadExited.connect(loop.quit)
+    project_download.startDownload()
+    loop.exec_()
+
+    plg_logger.log(message=f"Download of {uri} succeedeed", log_level=3)
+    return output_path
