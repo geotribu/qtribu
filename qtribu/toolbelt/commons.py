5e5dd90ef0840112b466eafd61b947f8f2e90c9d
gounux <contact@guilhemallaman.net>
2024-04-13 20:16:28 +0200

    Add docstring

--- qtribu/toolbelt/commons.py
+++ qtribu/toolbelt/commons.py
@@ -3,4 +3,12 @@ from qgis.PyQt.QtGui import QDesktopServices
 
 
 def open_url_in_browser(url: str) -> bool:
+    """Opens an url in a browser using user's desktop environment
+
+    :param url: url to open
+    :type url: str
+
+    :return: true if successful otherwise false
+    :rtype: bool
+    """
     return QDesktopServices.openUrl(QUrl(url))
