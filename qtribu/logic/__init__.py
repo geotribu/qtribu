9baf2ac21782783d7ec3504509a9d10f17754d4c
Julien M. <julien.moura@oslandia.com>
2024-08-01 22:47:30 +0200

    refactor(rss): move RSS reader to its own folder

--- qtribu/logic/__init__.py
+++ qtribu/logic/__init__.py
@@ -1,4 +1,3 @@
 #! python3  # noqa: E265
 from .custom_datatypes import RssItem  # noqa: F401
-from .rss_reader import RssMiniReader  # noqa: F401
 from .splash_changer import SplashChanger  # noqa: F401
