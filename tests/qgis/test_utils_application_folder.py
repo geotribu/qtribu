1c1d03693ee7a30e333c58e9252f5591b3acf95e
Julien M. <julien.moura@oslandia.com>
2024-08-01 22:42:21 +0200

    feature(utils): add util to locate application folder

---
+++ tests/qgis/test_utils_application_folder.py
@@ -0,0 +1,45 @@
+#! python3  # noqa E265
+
+"""
+    Usage from the repo root folder:
+
+    .. code-block:: bash
+        # for whole tests
+        python -m unittest tests.qgis.test_utils_application_folder
+        # for specific test
+        python -m unittest tests.qgis.test_utils_application_folder.TestToolbeltApplicationFolder.test_get_app_folder
+"""
+
+# standard library
+import unittest
+from os import getenv
+from pathlib import Path
+
+# project
+from oslandia.toolbelt.application_folder import _posixify, get_app_dir
+
+# ############################################################################
+# ########## Classes #############
+# ################################
+
+
+class TestToolbeltApplicationFolder(unittest.TestCase):
+    """Test toolbelt application folder module"""
+
+    def test_get_app_folder(self):
+        """Test application folder retrieval."""
+        self.assertEqual(
+            get_app_dir(dir_name="cache", roaming=False),
+            Path(getenv("LOCALAPPDATA", Path.home())).joinpath(".oslandia/cache"),
+        )
+
+    def test_posixify(self):
+        """Test posixify util."""
+        self.assertEqual(_posixify("test PoSiXiFieD string"), "test-posixified-string")
+
+
+# ############################################################################
+# ####### Stand-alone run ########
+# ################################
+if __name__ == "__main__":
+    unittest.main()
