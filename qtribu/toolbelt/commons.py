c089f665e5f1f40e095376a199659e67475dc63d
Julien M. <julien.moura@oslandia.com>
2024-08-02 16:06:50 +0200

    improve(commons): handle bad import of WebViewer

--- qtribu/toolbelt/commons.py
+++ qtribu/toolbelt/commons.py
@@ -1,9 +1,12 @@
 from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtGui import QDesktopServices
 
-from qtribu.logic.web_viewer import WebViewer
+try:
+    from qtribu.logic.web_viewer import WebViewer
 
-web_viewer = WebViewer()
+    web_viewer = WebViewer()
+except ImportError:
+    web_viewer = None
 
 
 def open_url_in_browser(url: str) -> bool:
@@ -27,6 +30,9 @@ def open_url_in_webviewer(url: str, window_title: str) -> None:
     :param window_title: title to give to the webviewer window
     :type window_title: str
     """
+    if web_viewer is None:
+        open_url_in_browser(url=url)
+
     web_viewer.display_web_page(url)
     if web_viewer.wdg_web:
         web_viewer.set_window_title(window_title)
