c213c2dfb15ae3a695af0c03a6d273663923b854
Julien M. <julien.moura@oslandia.com>
2024-08-29 22:47:01 +0200

    fix(import): QChatWidget

--- qtribu/plugin_main.py
+++ qtribu/plugin_main.py
@@ -18,11 +18,11 @@ from qgis.PyQt.QtWidgets import QAction
 # project
 from qtribu.__about__ import DIR_PLUGIN_ROOT, __icon_path__, __title__, __uri_homepage__
 from qtribu.constants import ICON_ARTICLE, ICON_GEORDP
+from qtribu.gui.dck_qchat import QChatWidget
 from qtribu.gui.dlg_contents import GeotribuContentsDialog
 from qtribu.gui.dlg_settings import PlgOptionsFactory
 from qtribu.gui.form_article import ArticleForm
 from qtribu.gui.form_rdp_news import RdpNewsForm
-from qtribu.gui.wdg_qchat import QChatWidget
 from qtribu.logic.news_feed.rss_reader import RssMiniReader
 from qtribu.logic.splash_changer import SplashChanger
 from qtribu.toolbelt import PlgLogger, PlgOptionsManager
