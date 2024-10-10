d24b16ed8dccce6e85fc2371840cd1f1935dd364
Julien M. <julien.moura@oslandia.com>
2024-10-10 22:45:49 +0200

    update(ui): display avatar right into the combobox

--- qtribu/constants.py
+++ qtribu/constants.py
@@ -192,18 +192,39 @@ GEORDP_NEWS_ICONS: tuple = (
 )
 
 # QChat
-QCHAT_NICKNAME_MINLENGTH = 3
-ADMIN_MESSAGES_NICKNAME = "admin"
-ADMIN_MESSAGES_AVATAR = "mIconWarning.svg"
-ERROR_MESSAGES_COLOR = "#ff0000"
-INTERNAL_MESSAGE_AUTHOR = "internal"
+QCHAT_NICKNAME_MINLENGTH: int = 3
+ADMIN_MESSAGES_NICKNAME: str = "admin"
+ADMIN_MESSAGES_AVATAR: str = "mIconWarning.svg"
+ERROR_MESSAGES_COLOR: str = "#ff0000"
+INTERNAL_MESSAGE_AUTHOR: str = "internal"
+QCHAT_USER_AVATARS: dict[str, str] = {
+    "Arrow Up": "mActionArrowUp.svg",
+    "Calculate": "mActionCalculateField.svg",
+    "Camera": "mIconCamera.svg",
+    "Certificate": "mIconCertificate.svg",
+    "Comment": "mIconInfo.svg",
+    "Compressed": "mIconZip.svg",
+    "Folder": "mIconFolder.svg",
+    "GeoPackage": "mGeoPackage.svg",
+    "GPU": "mIconGPU.svg",
+    "HTML": "mActionAddHtml.svg",
+    "Information": "mActionPropertiesWidget.svg",
+    "Network Logger": "mIconNetworkLogger.svg",
+    "Postgis": "mIconPostgis.svg",
+    "Python": "mIconPythonFile.svg",
+    "Pyramid": "mIconPyramid.svg",
+    "Raster": "mIconRaster.svg",
+    "Spatialite": "mIconSpatialite.svg",
+    "Tooltip": "mActionMapTips.svg",
+    "XYZ": "mIconXyz.svg",
+}
 
 # QChat cheatcodes
-CHEATCODE_DIZZY = "givemesomecheese"
-CHEATCODE_DONTCRYBABY = "dontcrybaby"
-CHEATCODE_IAMAROBOT = "iamarobot"
-CHEATCODE_10OCLOCK = "its10oclock"
-CHEATCODE_QGIS_PRO_LICENSE = "qgisprolicense"
+CHEATCODE_DIZZY: str = "givemesomecheese"
+CHEATCODE_DONTCRYBABY: str = "dontcrybaby"
+CHEATCODE_IAMAROBOT: str = "iamarobot"
+CHEATCODE_10OCLOCK: str = "its10oclock"
+CHEATCODE_QGIS_PRO_LICENSE: str = "qgisprolicense"
 
 CHEATCODES = [
     CHEATCODE_DIZZY,
